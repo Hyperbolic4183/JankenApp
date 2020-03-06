b4ff7ea10c762e19f0877c82b055d37c63bbfd99
Amane <otsuka.amane@gmail.com>
2020-03-06 15:07:34 +0900

    Initial Commit

---
+++ JankenApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JankenApp
+//
+//  Created by owner on 2020/03/06.
+//  Copyright © 2020 Amane.Otsuka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
